7fbb872f9bafa657d66a089f4c67781bf775dc56
Lamphrangmi-Lamin <lamphrangmilamin46169@gmail.com>
2025-03-13 08:05:32 +0530

    feat: set up Ract Vite project and configure Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import SearchBar from './components/Searchbar'
+
+function App() {
+
+  return (
+    <>
+      <SearchBar />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Searchbar.jsx
@@ -0,0 +1,9 @@
+function SearchBar() {
+    return (
+        <div className="bg-amber-400">
+            <p>Hello world</p>
+        </div>
+    )
+}
+
+export default SearchBar
