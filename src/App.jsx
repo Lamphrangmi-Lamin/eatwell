6df8771947285c217fd5e8f30cb64de5ed29fa4b
Lamphrangmi-Lamin <lamphrangmilamin46169@gmail.com>
2025-04-27 20:47:10 +0530

    feat: add Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import HomePage from './components/pages/HomePage';
 import SignupPage from './components/pages/SignupPage';
 import RecipeDetail from './components/RecipeDetail';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div>
-      <nav className='flex items-center justify-center gap-8 p-4'>
-          <Link to="/" className='text-[#FFBA08] hover:text-[#F48C06] transition-colors duration-300 text-lg font-semibold'>Home</Link>
-          <Link to="/signup" className='text-[#FFBA08] hover:text-[#F48C06] transition-colors duration-300 text-lg font-semibold'>Sign Up</Link>
-      </nav>
+      <Navbar />
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/signup' element={<SignupPage />} />
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+
+function Navbar() {
+    return (
+        <div className="flex justify-between items-center mb-4 mt-2">
+            <Link to="/" className='hover:text-[#F48C06] transition-colors duration-300'>
+              <i className="fas fa-home ml-4 text-xl"></i>
+            </Link>
+
+            <h1 className="text-2xl font-bold">Better Taste</h1>
+
+            <Link to="/signup" className='hover:text-[#F48C06] transition-colors duration-300 text-lg font-semibold'>
+              <i className="fas fa-user-circle mr-4 text-2xl"></i>
+            </Link>
+        </div>
+    )
+}
+
+export default Navbar;
